1645eee9d2bad38bcb6a04b580aaf480575f5ee7
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-07-08 14:11:31 +0530

    :truck: code updated.

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -30,7 +30,7 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const results: Result[] | undefined = data?.query?.pages;
 
   const content = (
-    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+    <main className="bg-slate-400 text-black mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
         Object.values(results).map((result) => {
           return <Item key={result.pageid} result={result} />;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1>hello,world!</h1>
+    <main className="min-h-screen text-black">
     </main>
   );
 }
